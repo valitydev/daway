b0308056006a3bd50f2de2773df51c55e906761c
Anatoly Karlov <karleowne@gmail.com>
2023-11-07 13:50:10 +0200

    MI-36: add withdrawal_session index

--- src/main/resources/db/migration/V17__change_withdrawal_adjustment_idx.sql
+++ src/main/resources/db/migration/V17__change_withdrawal_adjustment_idx.sql
@@ -1,1 +1,1 @@
-CREATE INDEX withdrawal_session_withdrawal_id_idx ON dw.withdrawal_session USING btree (withdrawal_id);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS withdrawal_session_withdrawal_id_idx ON dw.withdrawal_session USING btree (withdrawal_id);
