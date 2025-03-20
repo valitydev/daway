8f64b134bfdb85f9d8a592efd43cb1d9fc713a3a
ggmaleva <ggmaleva@yandex.ru>
2025-03-20 16:38:44 +0300

    fix review

--- src/main/resources/db/migration/V28__add_paуment_partition.sql
+++ src/main/resources/db/migration/V28__add_paуment_partition.sql
@@ -22,33 +22,49 @@ $$
             --- Таблица уже является партиционированной. Пропускаем партиционирование
             RETURN;
         ELSE
-            --- Таблица не является партиционированной. Создаем партиционированную таблицу и партиции для нее
-            --- 1. Переименовываем текущую таблицу в ***_old
-            EXECUTE format('ALTER TABLE %I RENAME TO %I_old', table_name, table_name);
+            --- Таблица не является партиционированной.
+            --- 1. Создаем партиционированную таблицу копию оригинальной с постфиксом _new.
+            --- Учитываем constraint с учетом поля партиционирования.
+            --- Продолжим использовать предыдущую последовательность (payment_id_seq), поэтому используем BIGINT вместо BIGSERIAL
+            EXECUTE format('CREATE TABLE IF NOT EXISTS dw.%I_new
+                            (
+                                id                              BIGINT                      NOT NULL DEFAULT nextval(''dw.payment_id_seq''::regclass),
+                                event_created_at                TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+                                invoice_id                      CHARACTER VARYING           NOT NULL,
+                                payment_id                      CHARACTER VARYING           NOT NULL,
+                                created_at                      TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+                                party_id                        CHARACTER VARYING           NOT NULL,
+                                shop_id                         CHARACTER VARYING           NOT NULL,
+                                domain_revision                 BIGINT                      NOT NULL,
+                                party_revision                  BIGINT,
+                                amount                          BIGINT                      NOT NULL,
+                                currency_code                   CHARACTER VARYING           NOT NULL,
+                                make_recurrent                  BOOLEAN,
+                                sequence_id                     BIGINT,
+                                change_id                       INTEGER,
+                                wtime                           TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE ''utc''::text),
+                                external_id                     character varying COLLATE pg_catalog."default",
+                                payment_flow_type               dw.payment_flow_type        NOT NULL,
+                                payment_flow_on_hold_expiration character varying COLLATE pg_catalog."default",
+                                payment_flow_held_until         timestamp without time zone,
 
-            --- 2. Добавляем в индексы поле партиционирования
-            --- Удаляем старые индексы
-            EXECUTE format('ALTER TABLE %I_old DROP CONSTRAINT %I_pkey;', table_name, table_name);
-            EXECUTE format('ALTER TABLE %I_old DROP CONSTRAINT %I_uniq;', table_name, table_name);
-            --- Создаем новые индексы с полем партиционирования
-            EXECUTE format('ALTER TABLE %I_old ADD CONSTRAINT %I_pkey PRIMARY KEY (id, %s);', table_name, table_name,
+                                CONSTRAINT %I_new_pkey PRIMARY KEY (id, %s),
+                                CONSTRAINT %I_new_uniq UNIQUE (invoice_id, payment_id, sequence_id, change_id, %s)
+                            ) PARTITION BY RANGE ( %s );',
+                           table_name,
+                           table_name,
+                           partition_field,
+                           table_name,
+                           partition_field,
                            partition_field);
-            EXECUTE format(
-                    'ALTER TABLE %I_old ADD CONSTRAINT %I_uniq UNIQUE (invoice_id, payment_id, sequence_id, change_id, %s);',
-                    table_name, table_name, partition_field);
-
-            --- 3. Создаем новую партиционированную таблицу с сохранением старого имени для обеспечения совместимости.
-            --- Также сохраняется структура и индексы таблицы оригинала.
-            EXECUTE format('CREATE TABLE %I (LIKE %I_old INCLUDING ALL) PARTITION BY RANGE ( %s );', table_name,
-                           table_name, partition_field);
 
 
-            --- 4. Создаем партиции
+            --- 2. Создаем партиции
             WHILE start_date < end_date
                 LOOP
                     partition_name := table_name || '_' || TO_CHAR(start_date, 'YYYY_MM');
                     EXECUTE format('
-                CREATE TABLE %I PARTITION OF %I FOR VALUES FROM (%L) TO (%L);',
+                CREATE TABLE %I PARTITION OF %I_new FOR VALUES FROM (%L) TO (%L);',
                                    partition_name,
                                    table_name,
                                    start_date,
