d2dfa33164caca9e0244a0fa5bf6284c1c0df67b
kostyastruga <koztyastruga@gmail.com>
2026-02-17 23:36:08 +0300

    Fix test

--- src/test/java/dev/vality/daway/config/PostgresqlJooqSpringBootITest.java
+++ src/test/java/dev/vality/daway/config/PostgresqlJooqSpringBootITest.java
@@ -13,6 +13,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @PostgresqlTestcontainerSingleton
 @JooqTest
-@SpringBootTest
 public @interface PostgresqlJooqSpringBootITest {
 }
